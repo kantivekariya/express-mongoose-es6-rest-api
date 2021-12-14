e291e3bf548c57242c7359772d7f34216c40cde2
kevit-kanti-vekariya <kanti.vekariya@kevit.io>
2021-12-14 16:14:00 +0530

    Update port number

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ connectMongo();
 
 app.use("/", mainRouter);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on isProductionss => ${isProduction}`);
   console.log(`Server is running on PORT ${PORT}`);
